f674b9e36d3d8baddc43dd59f1dd7519043bdc59
ChacoFon <leyatalks1010@gmail.com>
2025-05-15 23:13:16 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/leya-fronted/sw.js', { scope: '/leya-fronted/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
